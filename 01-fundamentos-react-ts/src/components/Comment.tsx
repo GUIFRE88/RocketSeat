908c215813c9a57eda0644702883e3654981c178
Guilherme Freudenburg <guilherme.fre073@gmail.com>
2023-01-31 07:16:00 -0300

    Projeto 01 RocketSeat Ignite

---
+++ 01-fundamentos-react-ts/src/components/Comment.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+import {Trash, ThumbsUp} from 'phosphor-react'
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({content, onDeleteComment}: CommentProps){
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment(){
+    setLikeCount((state)=> {
+       return state + 1;   
+    });
+  }
+
+
+  return(
+    <div className={styles.comment}>
+      <Avatar 
+        hasBorder={false}
+        src='https://avatars.githubusercontent.com/u/36928790?v=4'
+        alt=""
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilhrme Freudenbyrg</strong>
+              <time title='11 de Maio as oite e treze' dateTime='2022-05-11 08:00:00'>Cerca de uma 1h atrás</time>
+            </div>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+          <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+          </footer>
+        </div>
+      </div>
+    </div>
+  )
+  
+}
